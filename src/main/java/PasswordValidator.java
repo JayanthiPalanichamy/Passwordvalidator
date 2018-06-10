2466b2a81799836aec6bd3462f9a369e7dfd2316
Jayanthipalanichamy <jayanthi.palanichamy1997@gmail.com>
2018-06-10 21:56:33 +0530

    extracting password validation regex

--- src/main/java/PasswordValidator.java
+++ src/main/java/PasswordValidator.java
@@ -3,12 +3,14 @@ import java.util.regex.Pattern;
 
 public class PasswordValidator {
 
+    public static final String PASSWORD_VALIDATION_REGEX = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,15}$";
+
     public boolean isValid(String password) {
 
         if (password.length() == 0) {
             return false;
         } else {
-            Pattern pattern = Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,15}$");
+            Pattern pattern = Pattern.compile(PASSWORD_VALIDATION_REGEX);
             Matcher matcher = pattern.matcher(password);
             return matcher.find();
         }
